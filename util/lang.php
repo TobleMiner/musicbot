4069683273138a975a63c1ef944a064c93557d37
Steven Tappert <admin@dark-it.net>
2014-08-18 03:28:02 +0200

    fixed instance create

--- util/lang.php
+++ util/lang.php
@@ -1,30 +1,37 @@
 <?php
-	require_once("config/lang.php");
-	
+	require_once( __DIR__ . '/../config/lang.php' );
+
 	class Langdict
 	{
-		public static $instance = new Langdict();
+		/**
+		 * @var Langdict
+		 */
+		public static $instance = NULL;
 
 		private $dict;
 
 		public function __construct()
 		{
-			$this -> dict = Array();
+			$this->dict = Array();
 		}
 
 		public function addFile($file)
 		{
 			$lang = parse_ini_file($file);
-			if($lang === FALSE)
-				throw new Exception("Failed parsing langfile '$file'");
+			if ($lang === FALSE)
+			{
+				throw new Exception( "Failed parsing langfile '$file'" );
+			}
 
-			$this -> dict = array_merge($this -> dict, $lang);
+			$this->dict = array_merge($this->dict, $lang);
 		}
 
 		public function get($key)
 		{
-			if(array_key_exists($key, $this -> dict))
-				return $this -> dict[$key];
+			if (array_key_exists($key, $this->dict))
+			{
+				return $this->dict[$key];
+			}
 
 			return $key;
 		}
@@ -32,8 +39,9 @@
 
 	function trans($key)
 	{
-		return Langdict::$instance -> get($key);
+		return Langdict::$instance->get($key);
 	}
 
-	Langdict::$instance -> addFile("lang/main/" . LangConf::$langfile);
+	Langdict::$instance = new Langdict();
+	Langdict::$instance->addFile("lang/main/" . LangConf::$langfile);
 ?>
