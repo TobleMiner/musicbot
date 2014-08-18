f54d16c504cefd3e8b6968dd826d083a62cc4406
Tobias Schramm <tobleminer@gmail.com>
2014-08-18 13:59:00 +0200

    Fixed indentation and added valid result for invalid API calls

--- api.php
+++ api.php
@@ -2,8 +2,10 @@
 	if (array_key_exists("jsonapi", $_POST))
 	{
 		require_once( __DIR__ . "/api/apiJSON.php" );
-      if(array_key_exists('data', $_POST))
-		   echo json_encode(( new BotApiJSON )->call(json_decode($_POST["data"])));
+		if(array_key_exists('data', $_POST))
+			echo json_encode(( new BotApiJSON )->call(json_decode($_POST["data"])));
+		else 
+			echo json_encode(new CommandResult(FALSE, "JSON data missing", BotApi::API_MISSING_PARAMS));
 	}
 	else
 	{
